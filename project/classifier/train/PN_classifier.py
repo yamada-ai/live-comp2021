ffd950f76ca86672195c38411c07272d192f6c00
SInadumi <inazumi19@icloud.com>
2021-10-06 23:01:17 +0900

    Add get_wago_pn in PN_classifier

--- project/classifier/train/PN_classifier.py
+++ project/classifier/train/PN_classifier.py
@@ -23,18 +23,16 @@ class PN_Classifier():
         pn_list = []
         for pg in self.posi_list:
             try:
-                #print((self.vec_model.similarity(pg, _word) - 0.99) * (10000))
                 pn_list.append((self.vec_model.similarity(pg, _word) - 0.99) * (1000))
             except KeyError:
                 continue
         for ng in self.nega_list:
             try:
-                #print((self.vec_model.similarity(ng, _word)-0.99) * (-10000))
-                pn_list.append((self.vec_model.similarity(pg, _word) - 0.99)* (-1000))
+                pn_list.append((self.vec_model.similarity(ng, _word) - 0.99)* (-1000))
             except KeyError:
                 continue
         if len(pn_list) != 0:
-            res = sum(pn_list) / len(pn_list) + 0.21
+            res = sum(pn_list) / len(pn_list)
         # 名詞・形容詞などは値が小さくなるため調整が必要
         if res > 0.3:
             return 1
@@ -55,6 +53,22 @@ class PN_Classifier():
             res = self.get_unknownwords_score(_word)
         return res
 
+    def get_wago_pn_score(self, _word, _text, _score, _dict, _wago_dict):
+        wago = ""
+        score = 0
+        extract_word = []
+        if _word in _wago_dict:
+            key = ''
+            for t in _text:
+                key = t + key
+            if t in _dict:
+                extract_word.append([t, _dict[t]['score']])
+                # search wago
+                if key in _wago_dict[_word]:
+                    wago = key
+                    score = _wago_dict[_word][wago]['score']
+        return wago, score, extract_word
+
     def totaling_score(self, _scores):
         all_tot = 0
         """
@@ -63,26 +77,31 @@ class PN_Classifier():
         """
         for token in _scores:
             if 'REV' in token[0]:
-                print(all_tot)
                 all_tot *= token[1]
             else:
                 all_tot += token[1]
         return math.tanh(all_tot)
 
-    def get_document_pn_score(self, _doc, _dict, _wego_dict):
+    def get_document_pn_score(self, _doc, _dict, _wago_dict):
 
         # tokenize _doc
         lemmas = self.pre.get_lemma(_doc)[0]
         POSes = self.pre.get_POS(_doc)[0]
         # 変数の初期化
         scores = []
-        prevtext = ''
+        prevtext = []
         score = 0.
         tag = ''
-
         for index, token in enumerate(list(zip(lemmas,POSes))):
-            prevtext += token[0]
+            prevtext = [token[0]] + prevtext
             # 用言の極性
+            wago_tag, wago_score, extract = self.get_wago_pn_score(token[0], prevtext, scores, _dict, _wago_dict)
+            if wago_score != 0:
+                # 後処理
+                scores.append([wago_tag, wago_score])
+                for ex in extract:
+                    scores.remove(ex)
+                continue
 
             # 副詞による強調表現
             if token[1] == '副詞':
@@ -104,14 +123,14 @@ class PN_Classifier():
             if tag == '' or score == 0:
                 continue
             scores.append([tag, score])
-            tag = 0
-            score = ''
+            score = 0
+            tag = ''
         return self.totaling_score(scores), scores
     
-    def predict(self, _text, _dict, _wego_dict):
-        score, words = self.get_document_pn_score(_text, _dict, _wego_dict)
-        print(words)
-        print(score)
+    def predict(self, _text, _dict, _wago_dict):
+        score, words = self.get_document_pn_score(_text, _dict, _wago_dict)
+        # print(words)
+        # print(score)
         if score > self.P_Threshold :
             return 8    #'positive'
         elif score < self.N_Threshold:
